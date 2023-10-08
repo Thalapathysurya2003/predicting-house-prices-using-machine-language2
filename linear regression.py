cff311e893260cc5261fc48cf26c566f345171be
Thalapathysurya2003 <146306855+Thalapathysurya2003@users.noreply.github.com>
2023-10-08 07:34:00 +0200

    Create linear regression.py

---
+++ linear regression.py
@@ -0,0 +1,7 @@
+from sklearn.linear_model import LinearRegression
+ 
+model_LR = LinearRegression()
+model_LR.fit(X_train, Y_train)
+Y_pred = model_LR.predict(X_valid)
+ 
+print(mean_absolute_percentage_error(Y_valid, Y_pred))
